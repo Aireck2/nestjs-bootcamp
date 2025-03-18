aa6743188436afe5d63adaafd370a2c3eaa2f38b
@erick <erickescribaa@gmail.com>
2025-03-18 01:23:28 -0500

    feat(products): import modules

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -2,20 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { ProductsMsModule } from './products-ms.module';
 
+const PORT = Number(process.env.PRODUCTS_MS_PORT);
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     ProductsMsModule,
     {
       transport: Transport.TCP,
       options: {
-        port: Number(process.env.PRODUCTS_MS_PORT),
+        port: PORT,
         host: '0.0.0.0',
       },
     },
   );
   await app.listen();
-  console.log(
-    `Microservice::Products is listening on ${Number(process.env.PRODUCTS_MS_PORT)}`,
-  );
+  console.log(`Microservice::Products is listening on ${PORT}`);
 }
 bootstrap();
