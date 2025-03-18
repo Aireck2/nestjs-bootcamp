705a4e6944d2f5a95e4905cfd51be9ea96e7ab82
@erick <erickescribaa@gmail.com>
2025-03-18 01:22:44 -0500

    feat(gateway):  import modules

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductsModuleModule } from './products-module/products-module.module';
+import { OrdersModuleModule } from './orders-module/orders-module.module';
+import { UsersModuleModule } from './users-module/users-module.module';
+import { LoginHistoryModuleModule } from './login-history-module/login-history-module.module';
+import { AuthModuleModule } from './auth-module/auth-module.module';
 
 @Module({
-  imports: [ProductsModuleModule],
+  imports: [ProductsModuleModule, OrdersModuleModule, UsersModuleModule, LoginHistoryModuleModule, AuthModuleModule],
 })
 export class GatewayModule {}
--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 
+const PORT = Number(process.env.GATEWAY_PORT);
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(Number(process.env.GATEWAY_PORT));
+  await app.listen(PORT);
+  console.log(`Gateaway is listening on ${PORT}`);
 }
 bootstrap();
