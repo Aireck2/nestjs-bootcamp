6a64914a2f698a270334fae440769ffcedf4d6b0
@erick <erickescribaa@gmail.com>
2025-03-19 18:40:33 -0500

    feat(uers): add login_history entity

--- apps/users/src/login-history/entities/login-history.entity.ts
+++ apps/users/src/login-history/entities/login-history.entity.ts
@@ -1,1 +1,27 @@
-export class LoginHistory {}
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { UserEntity } from '../../users/entities/user.entity';
+
+@Entity('login_history')
+export class LoginHistory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.id, {
+    nullable: false,
+    eager: true,
+  })
+  @JoinColumn({ name: 'user_id' })
+  userId: number;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+}
