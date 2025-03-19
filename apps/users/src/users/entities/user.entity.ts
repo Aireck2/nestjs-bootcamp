62637da477f1a80288a7d408e538b444b92182ab
@erick <erickescribaa@gmail.com>
2025-03-19 18:40:19 -0500

    feat(users): add user entity

--- apps/users/src/users/entities/user.entity.ts
+++ apps/users/src/users/entities/user.entity.ts
@@ -1,1 +1,43 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn({ name: 'user_id' })
+  id: number;
+
+  @Column({ length: 30 })
+  name: string;
+
+  @Column({ length: 30 })
+  email: string;
+
+  @Column({ length: 30 })
+  password: string;
+
+  @Column({ length: 15 })
+  role: string;
+
+  @Column({ name: 'is_active', type: 'boolean', default: true })
+  isActive: boolean;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
