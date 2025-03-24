ca053c183e7a10707ca5dc576f0a4a4b4b24b433
@erick <erickescribaa@gmail.com>
2025-03-24 18:17:00 -0500

    feat(gateway): add swagger document builder

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GatewayModule } from './gateway.module';
 
 const PORT = Number(process.env.GATEWAY_PORT);
@@ -13,6 +14,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Gateaway API')
+    .setDescription('Gateaway API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(PORT);
   console.log(`Gateaway is listening on ${PORT}`);
 }
