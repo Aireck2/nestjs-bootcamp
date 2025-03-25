be53f21bc4f1fb0fe720367dd62c91affb24ff9b
@erick <erickescribaa@gmail.com>
2025-03-24 20:52:19 -0500

    fix: replace with bcryptjs

--- apps/users/src/auth/auth.service.ts
+++ apps/users/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../users/entities/user.entity';
 import { LoginDto } from './dto/login.dto';
